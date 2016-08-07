4ec8b15ffd410b6f63a8f417cdb0827ea7e7131f
mrweckel <mrweckel@gmail.com>
2016-08-07 13:23:28 -0400

    Adds project 6

---
+++ 06-Project_06/source/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+import StudentsContainer from './StudentsContainer';
+
+
+
+
+render(<StudentsContainer />, document.getElementById('root'));
